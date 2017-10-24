2b035d60305cd80b1d50d18b678c6fa25ace4b25
unknown <161710116@smkassalaambandung.sch.id>
2017-10-24 17:40:03 +0700

    latihan 4

---
+++ gr1.php
@@ -0,0 +1,14 @@
+<?php
+
+class manusia {
+
+	public $suara;
+	public $berat;
+
+	public function __construct($suara,$berat){
+		$this->suara = $suara;
+		$this->berat = $berat;
+	}
+	
+}
+?>
---
+++ gr2.php
@@ -0,0 +1,10 @@
+<center>
+<?php
+require_once'gr1.php';
+
+$m1 = new manusia("auuuoooooooooo",78);
+echo "Suaranya adalah ".$m1->suara.'<br>';
+echo "Beratnya adalah ".$m1->berat.'<br>';
+//echo "Melahirkan di ".$bio1->tempat_lahir.'<br>';
+?>
+</center>
